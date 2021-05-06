9939573a4ca15c5a97370e25d9befddc23e3c443
Su T. Nguyen <vnntsu@gmail.com>
2021-05-06 14:11:36 +0700

    [1] Insert break line

--- ecommerce-ios/ContentView.swift
+++ ecommerce-ios/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+
     var body: some View {
         Text("Hello, world!")
             .padding()
@@ -15,6 +16,7 @@ struct ContentView: View {
 }
 
 struct ContentView_Previews: PreviewProvider {
+
     static var previews: some View {
         ContentView()
     }
