25ff2680dbe544dd91a669e0712b25ad52f25b4a
Tam Nguyen Minh <nmint8m@gmail.com>
2021-05-17 17:17:02 +0700

    [22] Remove unused file

--- ecommerce-ios/Extensions/Color+Application.swift
+++ ecommerce-ios/Extensions/Color+Application.swift
@@ -32,7 +32,13 @@ extension Color {
 
     // MARK: - Initialize
 
-    init(hex: Int, alpha: Double = 1.0) {
-        self.init(UIColor(hex: hex, alpha: alpha).cgColor)
+    init(hex: UInt, alpha: Double = 1.0) {
+        self.init(
+            .sRGB,
+            red: Double((hex >> 16) & 0xff) / 255,
+            green: Double((hex >> 08) & 0xff) / 255,
+            blue: Double((hex >> 00) & 0xff) / 255,
+            opacity: alpha
+        )
     }
 }
