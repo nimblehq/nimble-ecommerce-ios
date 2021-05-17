1795ada0179e44da1548fdb1cac8ac9144b3c3ba
Bliss Pisit Wetcha <pisit@nimblehq.co>
2021-05-17 17:18:58 +0700

    [10] fix pregen code

--- ecommerce-iosWidget/EcommerceiOSWidget.swift
+++ ecommerce-iosWidget/EcommerceiOSWidget.swift
@@ -23,13 +23,7 @@ struct Provider: IntentTimelineProvider {
     func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> Void) {
         var entries: [SimpleEntry] = []
 
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, configuration: configuration)
-            entries.append(entry)
-        }
+        entries.append(SimpleEntry(date: Date(), configuration: configuration))
 
         let timeline = Timeline(entries: entries, policy: .atEnd)
         completion(timeline)
@@ -52,9 +46,9 @@ struct ecommerceiOSWidgetEntryView: View {
 }
 
 @main
-struct ecommerceiOSWidget: Widget {
+struct EcommerceiOSWidget: Widget {
 
-    let kind: String = "ecommerceiOSWidget"
+    let kind: String = "EcommerceiOSWidget"
 
     var body: some WidgetConfiguration {
         IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
@@ -65,7 +59,7 @@ struct ecommerceiOSWidget: Widget {
     }
 }
 
-struct ecommerce_iosWidget_Previews: PreviewProvider {
+struct Ecommerce_iosWidget_Previews: PreviewProvider {
 
     static var previews: some View {
         ecommerceiOSWidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
