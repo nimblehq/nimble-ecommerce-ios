40bed5b303e13cb4d19b69be1754c9dc9d0121c2
Su T. Nguyen <vnntsu@gmail.com>
2021-05-18 16:56:23 +0700

    [7] Change HStack to LazyHStack

--- ecommerce-ios/Sources/Home/View/CategoryView/CategoryCollectionView.swift
+++ ecommerce-ios/Sources/Home/View/CategoryView/CategoryCollectionView.swift
@@ -13,7 +13,7 @@ struct CategoryCollectionView: View {
     
     var body: some View {
         ScrollView(.horizontal, showsIndicators: false) {
-            HStack(alignment: .top, spacing: 8.0) {
+            LazyHStack(alignment: .top, spacing: 8.0) {
                 ForEach(categories) { category in
                     CategoryView(category: category)
                         .frame(width: 80.0, height: 88.0)
