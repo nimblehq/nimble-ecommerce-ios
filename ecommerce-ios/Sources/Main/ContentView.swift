cff884c6c5eff2c51120659373aacd43228e10c5
Tam Nguyen Minh <nmint8m@gmail.com>
2021-05-19 22:31:49 +0700

    [49] Implement the product color selection section on the Product Detail screen

--- ecommerce-ios/Sources/Main/ContentView.swift
+++ ecommerce-ios/Sources/Main/ContentView.swift
@@ -10,7 +10,13 @@ import SwiftUI
 struct ContentView: View {
 
     var body: some View {
-        TabbarView()
+        ColorSelectionView(cellViewModels: [
+            ColorCellViewModel(id: "pink", name: "pink", colorCode: "E40046"),
+            ColorCellViewModel(id: "indigo", name: "indigo", colorCode: "534ACC"),
+            ColorCellViewModel(id: "honey", name: "honey", colorCode: "FF8200"),
+            ColorCellViewModel(id: "gray", name: "gray", colorCode: "858999"),
+        ])
+        .padding(.horizontal, 17.0)
     }
 }
 
