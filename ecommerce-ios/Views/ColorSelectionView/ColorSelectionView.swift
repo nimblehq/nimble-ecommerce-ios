ef6d4cfc8e11f25f491385439076ea5f5762ac02
Bliss Pisit Wetcha <pisit@nimblehq.co>
2021-05-21 18:27:23 +0700

    [55] reverse unrelated files

--- ecommerce-ios/Views/ColorSelectionView/ColorSelectionView.swift
+++ ecommerce-ios/Views/ColorSelectionView/ColorSelectionView.swift
@@ -68,7 +68,7 @@ struct ColorSelectionView_Previews: PreviewProvider {
             ColorCellViewModel(id: "pink", name: "pink", colorCode: "E40046"),
             ColorCellViewModel(id: "indigo", name: "indigo", colorCode: "534ACC"),
             ColorCellViewModel(id: "honey", name: "honey", colorCode: "FF8200"),
-            ColorCellViewModel(id: "gray", name: "gray", colorCode: "858999")
+            ColorCellViewModel(id: "gray", name: "gray", colorCode: "858999"),
         ])
         .padding(.horizontal, 17.0)
     }
